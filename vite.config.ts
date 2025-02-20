d746a5f6e764bee0a6afbc4555b96d684fb7f200
Jeff <JeffreyGHJ@gmail.com>
2025-02-20 17:51:21 -0500

    try optimizations: add vite compression, pwa, chunksplit, remove package-lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    chunkSplitPlugin({
+      strategy: "unbundle", // Auto-splits large chunks
+    }),
+    viteCompression({
+      algorithm: "brotliCompress", // Or 'gzip'
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg}"],
+      },
+      manifest: {
+        name: "My Vite PWA",
+        short_name: "VitePWA",
+        theme_color: "#ffffff",
+        // icons: [
+        //   {
+        //     src: "/icon-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+        // ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
